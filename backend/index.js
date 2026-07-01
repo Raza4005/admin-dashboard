22d523afea9d71f7d4836ce85ee7be65bc5f2166
Mohsin Raza <razach1932@gmail.com>
2026-07-01 13:11:54 +0500

    Update part completed

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,8 @@ if(result){
 }
 })
 
+
+
 app.delete("/delete/:id", async (req, resp) => {
     const db = await connection();
     const id = req.params.id;
@@ -41,6 +43,33 @@ app.delete("/delete/:id", async (req, resp) => {
     const result = await collection.deleteOne({ _id: new ObjectId(id) })
     if (result) {
         resp.send({ message: 'task deleted ', success: true, result })
+    } else {
+        resp.send({ message: 'error try after sometime', success: false })
+    }
+})
+
+
+app.put("/update-task", async (req, resp) => {
+    const db = await connection();
+    const collection = await db.collection(collectionName);
+    const {_id,...fields} = req.body;
+    const update = {$set: fields};
+    console.log(fields);
+    const result = await collection.updateOne({_id: new ObjectId(_id)}, update);
+    if (result) {
+        resp.send({ message: 'task updated', success: true, result }) 
+    } else {
+        resp.send({ message: 'error try after sometime', success: false })
+    }
+})
+
+app.get("/task/:id", async (req, resp) => {
+    const db = await connection();
+    const id = req.params.id;
+    const collection = await db.collection(collectionName);
+    const result = await collection.findOne({ _id: new ObjectId(id) })
+    if (result) {
+        resp.send({ message: 'task found ', success: true, result })
     } else {
         resp.send({ message: 'error try after sometime', success: false })
     }
--- frontend/src/components/UpdateTask.jsx
+++ frontend/src/components/UpdateTask.jsx
@@ -1,21 +1,52 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export default function UpdateTask() {
-    const [taskData, setTaskData] = useState({});
+    const [taskData, setTaskData] = useState({
+        title: "",
+        description: ""
+    });
     const navigate = useNavigate();
+    const {id} = useParams()
+
+    useEffect(()=>{
+    getTask(id)
+    },[])
+
+    const getTask = async(id) =>{
+    let task = await fetch(`http://localhost:3200/task/`+id);
+    task = await task.json()
+    if(task.result){
+    setTaskData(task.result)
+    }
+}
+const updateTask = async () => {
+  console.log("function called", taskData);
+  let task = await fetch("http://localhost:3200/update-task", {
+    method: 'put',
+    body: JSON.stringify(taskData),
+    headers: {
+    'Content-Type': 'Application/Json'
+    }
+  });
+
+  task = await task.json()
+  if (task) {
+    navigate('/')
+  }
+}
 
     return (
         <div className="container">
             <h1>Update Task</h1>
 
             <label htmlFor="Title">Title</label>
-            <input onChange={(event) => setTaskData({...taskData, title: event.target.value})} />
+            <input value = {taskData.title} onChange={(event) => setTaskData({...taskData, title: event.target.value})} />
 
             <label htmlFor="Description">Description</label>
-            <textarea onChange={(event) => setTaskData({...taskData, description: event.target.value})} />
+            <textarea value = {taskData.description} onChange={(event) => setTaskData({...taskData, description: event.target.value})} />
 
-            <button className="submit">Update Task</button>
+            <button onClick={updateTask} className="submit">Update Task</button>
         </div>
     );
 }
